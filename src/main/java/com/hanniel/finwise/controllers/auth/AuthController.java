bb1ce16b92e6cdc9be701217e39982938ae75645
hanniel09 <hannielvieira1227@gmail.com>
2025-08-09 02:24:38 -0300

    feat: created AuthController.

---
+++ src/main/java/com/hanniel/finwise/controllers/auth/AuthController.java
@@ -0,0 +1,32 @@
+package com.hanniel.finwise.controllers.auth;
+
+import com.hanniel.finwise.domains.auth.AuthResponse;
+import com.hanniel.finwise.domains.auth.LoginRequest;
+import com.hanniel.finwise.domains.auth.RegisterRequest;
+import com.hanniel.finwise.services.auth.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        AuthResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
