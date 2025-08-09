aa846ffdbe32c925dadba241340d1cc565ba7f9f
hanniel09 <hannielvieira1227@gmail.com>
2025-08-09 02:12:09 -0300

    feat: create User entity subdivided into two users, UserCredentials and UserProfile.

---
+++ src/main/java/com/hanniel/finwise/domains/auth/UserCredentials.java
@@ -0,0 +1,85 @@
+package com.hanniel.finwise.domains.auth;
+
+import com.hanniel.finwise.domains.users.UserProfile;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user_credentials")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class UserCredentials implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Email
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+
+    @OneToOne(mappedBy = "credentials",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            fetch = FetchType.LAZY)
+    private UserProfile profile;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.role == Role.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
---
+++ src/main/java/com/hanniel/finwise/domains/users/UserProfile.java
@@ -0,0 +1,71 @@
+package com.hanniel.finwise.domains.users;
+
+import com.hanniel.finwise.domains.auth.UserCredentials;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Table
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "id")
+    private UserCredentials credentials;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String lastName;
+
+    @Min(13)
+    private int age;
+
+    private String occupation;
+
+    @DecimalMin("0.00")
+    @Column(precision = 10, scale = 2)
+    private BigDecimal salary;
+
+    @DecimalMin("0.00")
+    @Column(precision = 10, scale = 2)
+    private BigDecimal fixedIncome;
+
+    @DecimalMin("0.00")
+    @Column(precision = 10, scale = 2)
+    private BigDecimal monthlyBudget;
+
+    @Enumerated(EnumType.STRING)
+    private Currency defaultCurrency;
+
+    @Enumerated(EnumType.STRING)
+    private RiskProfile riskProfile;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
